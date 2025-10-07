c1fc9f1ee1e87eb9dde9dc29270647cade3a5f7f
Armaan Grewal <armaan@Armaans-MacBook-Air-2.local>
2025-10-07 16:36:04 -0400

    format fix

--- src/parse_data.py
+++ src/parse_data.py
@@ -66,84 +66,88 @@ def read_season(soup):
     season = os.path.basename(hrefs[1]).split("_")[0]
     return season
 
-
-# %%
-base_cols = None
-games = []
-for box_score in box_scores:
-    #using parse_html, open the directory and return html
-    soup = parse_html(box_score)
-    
-    #get the table of game score
-    line_score = read_line_score(soup)
-    teams = list(line_score["team"])
-    
-    summaries = [] 
-    for team in teams:
-        basic = read_stats(soup, team, "basic")
-        advanced = read_stats(soup, team, "advanced")
-    
-        #take the last row of basic and advanced and concatinate it in one single row for machine learning reading purposes
-        total = pd.concat([basic.iloc[-1,:], advanced.iloc[-1,:]])
-        total.index = total.index.str.lower()
-    
-    
-        #get the max stat of each category and structure it the same as above 
-        maxes = pd.concat([basic.iloc[:-1,:].max(), advanced.iloc[:-1,:].max()])
-        maxes.index = maxes.index.str.lower() + "_max"
-    
-        summary = pd.concat([total, maxes])
-    
-        if base_cols is None:
-            #remove duplicates from stats in both basic and advanced, keep first occurance of duplicates
-            base_cols = list(summary.index.drop_duplicates(keep="first"))
-            #remove bpm stat
-            base_cols = [b for b in base_cols if "bpm" not in b]
-    
-        #only keep index's that are in base_cols 
-        summary = summary[base_cols]
-    
-        #combine both teams summary into list
-        summaries.append(summary)
+def main()
+    # %%
+    base_cols = None
+    games = []
+    for box_score in box_scores:
+        #using parse_html, open the directory and return html
+        soup = parse_html(box_score)
         
-    #merge both teams summaries into one to make it into one dataframe
-    full_sum = pd.concat(summaries, axis=1).T
-    
-    #concat game score
-    game = pd.concat([full_sum, line_score], axis = 1)
-    
-    #home vs away team
-    game["home"] = [0,1]
-    
-    #make first row the second, for opponents row
-    game_opp = game.iloc[::-1].reset_index()
-    
-    game_opp.columns += "_opp"
-    
-    full_game = pd.concat([game, game_opp], axis =1)
-    
-    #get year of game
-    full_game["season"] = read_season(soup)
-    
-    #first 8 charectors gives the date
-    full_game["date"] = os.path.basename(box_score)[:8]
-    full_game["date"] = pd.to_datetime(full_game["date"], format="%Y%m%d")
-    
-    #who won specified by true/false
-    full_game["won"] = full_game["total"] > full_game["total_opp"]
-    games.append(full_game)
-    
+        #get the table of game score
+        line_score = read_line_score(soup)
+        teams = list(line_score["team"])
+        
+        summaries = [] 
+        for team in teams:
+            basic = read_stats(soup, team, "basic")
+            advanced = read_stats(soup, team, "advanced")
+        
+            #take the last row of basic and advanced and concatinate it in one single row for machine learning reading purposes
+            total = pd.concat([basic.iloc[-1,:], advanced.iloc[-1,:]])
+            total.index = total.index.str.lower()
+        
+        
+            #get the max stat of each category and structure it the same as above 
+            maxes = pd.concat([basic.iloc[:-1,:].max(), advanced.iloc[:-1,:].max()])
+            maxes.index = maxes.index.str.lower() + "_max"
+        
+            summary = pd.concat([total, maxes])
+        
+            if base_cols is None:
+                #remove duplicates from stats in both basic and advanced, keep first occurance of duplicates
+                base_cols = list(summary.index.drop_duplicates(keep="first"))
+                #remove bpm stat
+                base_cols = [b for b in base_cols if "bpm" not in b]
+        
+            #only keep index's that are in base_cols 
+            summary = summary[base_cols]
+        
+            #combine both teams summary into list
+            summaries.append(summary)
+            
+        #merge both teams summaries into one to make it into one dataframe
+        full_sum = pd.concat(summaries, axis=1).T
+        
+        #concat game score
+        game = pd.concat([full_sum, line_score], axis = 1)
+        
+        #home vs away team
+        game["home"] = [0,1]
+        
+        #make first row the second, for opponents row
+        game_opp = game.iloc[::-1].reset_index()
+        
+        game_opp.columns += "_opp"
+        
+        full_game = pd.concat([game, game_opp], axis =1)
+        
+        #get year of game
+        full_game["season"] = read_season(soup)
+        
+        #first 8 charectors gives the date
+        full_game["date"] = os.path.basename(box_score)[:8]
+        full_game["date"] = pd.to_datetime(full_game["date"], format="%Y%m%d")
+        
+        #who won specified by true/false
+        full_game["won"] = full_game["total"] > full_game["total_opp"]
+        games.append(full_game)
+        
 
-    if len(games) %100 == 0:
-        print(f"{len(games)}/{len(box_scores)}")
+        if len(games) %100 == 0:
+            print(f"{len(games)}/{len(box_scores)}")
 
-# %%
-games_df = pd.concat(games, ignore_index = True)
+    # %%
+    games_df = pd.concat(games, ignore_index = True)
 
-# %%
-#games_df
+    # %%
+    #games_df
 
-# %%
-games_df.to_csv("nba_games.csv")
+    # %%
+    games_df.to_csv("nba_games.csv")
 
-# %%
+    # %%
+
+
+if __name__ == "__main__":
+    main()
